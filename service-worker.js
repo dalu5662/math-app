1b7dc1cdcc18f979f77698727dfe6b8b9d411ce7
dalu5662 <dlzhang@tongji.edu.cn>
2026-03-22 18:49:43 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'math-app-v1';
+const urlsToCache = [
+  '/math-app/',
+  '/math-app/index.html',
+  '/math-app/manifest.json',
+  '/math-app/math-pwa-perfect/icon-192.png',
+  '/math-app/math-pwa-perfect/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
